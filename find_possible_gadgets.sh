483bd3389088e05cc389514a8d24f3692b38b15b
emanuel <134736553+EmanuelM153@users.noreply.github.com>
2026-04-28 22:02:09 -0500

    Refactorizations
    - Removed check of `-s` and `-u` flag used simultaneously

--- find_possible_gadgets.sh
+++ find_possible_gadgets.sh
@@ -63,54 +63,66 @@ then
         exit 0
 fi
 
+
+
 pattern=${patterns[0]}
 coincidences=$(grep -B $context -E "ret$" $file | grep -E "$pattern")
 addresses=$(echo "$coincidences" | sed "s/:.*//g")
 
-if [ $show_unique_coincidences -eq 0 ]
+if [ $show_unique_coincidences -eq 1 ]
 then
-        for addr in $addresses
-        do
-                gadget=$(grep --color=always -B $BEFORE_PATTERN -A $context "^[ ]*$addr" $file)
-
-                matches_next="yes"
-                i=1
-                while [ $i -lt $patterns_len ]
-                do
-                        pattern=${patterns[i]}
-                        expected_line=$(echo "$gadget" | awk "NR == $(($BEFORE_PATTERN + $i + 1))")
-                        matches_next=$(echo "$expected_line" | grep -E "$pattern")
-                        if [ -z "$matches_next" ]
-                        then
-                                break
-                        fi
-
-                        ((i++))
-                done
-
-                if [ -n "$matches_next" ]
-                then
-                        if [ $show_coincidences -eq 1 ]
-                        then
-                                echo "$gadget" | \
-                                        awk "NR > $BEFORE_PATTERN && NR <= $(($patterns_len + $BEFORE_PATTERN))"
-                        else
-                                ret_line_num=$(echo "$gadget" | \
-                                                awk "NR > $BEFORE_PATTERN && /ret$/{print NR; exit}")
-                                echo "$gadget" | awk "NR >= 1 && NR <= ${ret_line_num}"
-                        fi
-
-                        echo -e "--------------------------------------------------------------------------------\n"
-                fi
-        done
-else
-        if [ $show_coincidences -eq 0 ]
-        then
-                offset=$(echo "$coincidences" | head -n 1 | \
-                        awk 'match($0, /^.*:/) {print RLENGTH-1}')
-                echo "$coincidences" | uniq -c -f $offset
-        else
-                echo $usage
-                exit 1
-        fi
+        offset=$(echo "$coincidences" | head -n 1 | \
+                awk 'match($0, /^.*:/) {print RLENGTH-1}')
+        echo "$coincidences" | uniq -c -f $offset
+        exit 0
 fi
+
+
+function check_gadget_validity()
+{
+        gadget="$1"
+
+        match="yes"
+        i=1
+        while [ $i -lt $patterns_len ]
+        do
+                pattern=${patterns[i]}
+                expected_line=$(echo "$gadget" | awk "NR == $(($BEFORE_PATTERN + $i + 1))")
+                match=$(echo "$expected_line" | grep -E "$pattern")
+                if [ -z "$match" ]
+                then
+                        break
+                fi
+
+                ((i++))
+        done
+
+        if [ -n "$match" ]
+        then
+                echo 1
+        else
+                echo 0
+        fi
+}
+
+for addr in $addresses
+do
+        gadget=$(grep --color=always -B $BEFORE_PATTERN -A $context "^[ ]*$addr" $file)
+
+        valid_gadget=$(check_gadget_validity "$gadget")
+
+        if [ $valid_gadget -eq 1 ]
+        then
+                if [ $show_coincidences -eq 1 ]
+                then
+                        echo "$gadget" | \
+                                awk "NR > $BEFORE_PATTERN && NR <= $(($patterns_len + $BEFORE_PATTERN))"
+                else
+                        ret_line_num=$(echo "$gadget" | \
+                                        awk "NR > $BEFORE_PATTERN && /ret$/{print NR; exit}")
+                        echo "$gadget" | awk "NR >= 1 && NR <= ${ret_line_num}"
+                fi
+
+                echo -e "--------------------------------------------------------------------------------\n"
+        fi
+done
